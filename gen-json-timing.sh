d3f32cf8d38c3a7e77c6c94764d95db0388638b4
Carlos Rueda Morales <crueda@gmail.com>
2016-07-29 08:18:57 +0200

    aa

--- gen-json-timing.sh
+++ gen-json-timing.sh
@@ -33,8 +33,8 @@ LOG_FOR_ROTATE = 10
 
 entry_url = config['ENTRY_URL']
 
-stages_date = [config['S3_DATE'], config['S2_DATE'], config['S1_DATE']]
-stages_url = [config['S3_URL'],config['S2_URL'],config['S1_URL']]
+stages_date = [config['S24_DATE'], config['S23_DATE'], config['S22_DATE'], config['S21_DATE'], config['S20_DATE'], config['S19_DATE'], config['S18_DATE'], config['S17_DATE'], config['S16_DATE'], config['S15_DATE'], config['S14_DATE'], config['S13_DATE'], config['S12_DATE'], config['S11_DATE'], config['S10_DATE'], config['S9_DATE'], config['S9_DATE'], config['S7_DATE'], config['S6_DATE'], config['S5_DATE'], config['S4_DATE'], config['S3_DATE'], config['S2_DATE'], config['S1_DATE']]
+stages_url = [config['S24_URL'],config['S23_URL'],config['S22_URL'],config['S21_URL'],config['S20_URL'],config['S19_URL'],config['S18_URL'],config['S17_URL'],config['S16_URL'],config['S15_URL'],config['S14_URL'],config['S13_URL'],config['S12_URL'],config['S11_URL'],config['S10_URL'],config['S9_URL'],config['S8_URL'],config['S7_URL'],config['S6_URL'],config['S5_URL'],config['S4_URL'],config['S3_URL'],config['S2_URL'],config['S1_URL']]
 
 PID = "/var/run/json-generator-timing"
 
@@ -81,6 +81,11 @@ pidfile.write(str(os.getpid()))
 pidfile.close()
 #########################################################################
 
+class Competitor(object): 
+    nr = "" 
+    stime = "" 
+    epoch_time = 0 
+
 def getUTC():
 	t = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
 	return int(t)
@@ -88,14 +93,19 @@ def getUTC():
 def getEpoch(isoDate):
 	return (calendar.timegm(time.strptime(isoDate, '%Y-%m-%dT%H:%M:%SZ'))) * 1000
 
+def getEpochTime(isoDate):
+	return (calendar.timegm(time.strptime(isoDate, '%H:%M.%S')))
+
 def getActualStage(stages_epoch):
 	epoch_time = int(time.time()) * 1000
+	# corregir para el rally
+	epoch_time = epoch_time + 3600000
 	index = 0
 	for s in stages_epoch:
 		if (epoch_time > (s - 300000)):
 			return index
 		index += 1
-	return 0
+	return 23
 
 
 def getEntryList():
@@ -118,6 +128,8 @@ def getEntryList():
 
 def genTiming(url):
 	array_list = []
+	competitor_list = []
+	result_list = []
 
 	headers = {"Content-type": "application/json"}	
 	try:
@@ -138,11 +150,29 @@ def genTiming(url):
 			nr = s.attributes['nr'].value
 			stime = s.attributes['time'].value
 
-			competitor = {"type": "car_timing", "properties": {"pos": int(pos), "nr": int(nr), "driver": entryList[str(nr)], "diff": stime}}
-			array_list.append(competitor)
+			competitor = Competitor() 
+			competitor.nr = nr
+			competitor.stime = stime
+			competitor.epoch_time = getEpochTime(stime)
 
-		#with open('/var/www2/timing.json', 'w') as outfile:
-		with open('./timing.json', 'w') as outfile:
+			competitor_list.append(competitor)
+			'''
+			try:
+				competitor = {"type": "car_timing", "properties": {"pos": int(pos), "nr": str(nr), "driver": entryList[str(nr)], "diff": stime}}
+			except e:
+				competitor = {"type": "car_timing", "properties": {"pos": 0, "nr": 0, "driver": entryList[str(nr)], "diff": '--'}}
+			array_list.append(competitor)
+			'''
+		result_list = sorted(competitor_list, key=lambda competitor: competitor.epoch_time) 
+		position = 1
+		for r in result_list:
+			competitorOrdered = {"type": "car_timing", "properties": {"pos": position, "nr": r.nr, "driver": entryList[str(r.nr)], "diff": r.stime}}
+			array_list.append(competitorOrdered)
+			position += 1
+
+		#with open('/var/www2/timing.json', 'w') as outfile:		
+		with open('/Applications/MAMP/htdocs/wrc-hs/timing.json', 'w') as outfile:
+		#with open('./timing.json', 'w') as outfile:
 			json.dump(array_list, outfile)
 
 	except requests.ConnectionError as e:
